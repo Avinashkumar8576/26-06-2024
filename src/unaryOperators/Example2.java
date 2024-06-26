33b95f07726a756ab7177a55b1aa6527743c2180
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-06-26 21:46:03 +0530

    first commit

---
+++ src/unaryOperators/Example2.java
@@ -0,0 +1,19 @@
+package unaryOperators;
+
+public class Example2 {
+
+	public static void main(String[] args)
+	{
+
+		
+		int a = 30, b = 40, c = 10, d = 60;
+
+		int result10 = a++ + --d - --a + d-- - --b + ++a - --b;
+					//30 + 59 - 30 + 59 - 39 + 31 - 38 = 72 
+		
+		System.out.println(result10);
+
+	}
+
+}
+
